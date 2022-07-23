9b0765d6c5d6b4487a0ebfae743e81058b84f896
rahmaevao <rahmaevao@kpm-ritm.ru>
2022-07-23 16:29:16 +0300

    Реализовано удаление тега
    Closes #1

--- image.py
+++ image.py
@@ -1,3 +1,5 @@
+import copy
+
 import exif
 import PIL
 from loguru import logger
@@ -23,7 +25,7 @@ class Image:
         with open(self.__file_name, "wb") as image_file:
             image_file.write(self.__exif.get_file())
 
-    def add_tag(self, new_tag: Tag):
+    def add_tag(self, new_tag: Tag) -> None:
         current_description = self.__get_image_description()
         current_description.tags.append(
             TagModel(
@@ -35,6 +37,21 @@ class Image:
             )
         )
         self.add_image_description(current_description)
+
+    def remove_tag(self, tag_name) -> None:
+        current_description = self.__get_image_description()
+        new_description = ImageDescription()
+        for tag in current_description.tags:
+            if tag.name != tag_name:
+                new_description.tags.append(tag)
+        self.add_image_description(new_description)
+
+    def get_tags_name(self) -> list[str]:
+        try:
+            description = ImageDescription.parse_raw(str(self.__exif.image_description))
+            return [tag.name for tag in description.tags]
+        except Exception:
+            return []
 
     def __get_image_description(self) -> ImageDescription:
         try:
--- main.py
+++ main.py
@@ -15,6 +15,13 @@ def add_tag():
     image.add_tag(Tag(name=tag_name_text_input, frame=frame))
 
 
+def remove_tag():
+    global image
+    global selected_tag
+
+    image.remove_tag(selected_tag)
+
+
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
 image = Image("tests/images/ninja_turtles.jpeg")
@@ -23,7 +30,11 @@ cropped_img = st_cropper(image.pil_image, realtime_update=True, return_type="box
 
 frame = FrameModel.parse_obj(cropped_img)
 st.sidebar.markdown(image.get_information())
-tag_name_text_input = st.sidebar.text_input("Tag name", "")
+
+selected_tag = st.sidebar.selectbox("Choose the tag", image.get_tags_name())
+st.sidebar.button("Remove", on_click=remove_tag)
+
+tag_name_text_input = st.sidebar.text_input("Create tag", "")
 button = st.sidebar.button("Add tag", on_click=add_tag)
 logger.info(f"{frame=}")
 # st.sidebar.markdown(image.get_information())
